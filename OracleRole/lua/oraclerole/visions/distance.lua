8114c0b61e119b1240952e4c83db2d3886558b6f
squidserv <mprovost1992@gmail.com>
2021-10-11 18:30:38 -0400

    Initial commit

---
+++ OracleRole/lua/oraclerole/visions/distance.lua
@@ -0,0 +1,36 @@
+local VISION = {}
+
+VISION.Name = "A player is how far away?"
+VISION.id = "distance"
+
+CreateConVar("ttt_oracle_distance_unit", 0, FCVAR_NONE, "The distance reported in ft (0) or m (1)")
+
+function VISION:GetVision()
+    local p = self:GetRandomEnemy()
+
+    local m = GetConVar("ttt_oracle_distance_unit"):GetBool()
+
+    local distance = "0"
+    if p ~= nil then
+        ply = player.GetLivingRole(ROLE_ORACLE)
+        if m then
+            distance = math.ceil((ply:GetPos():Distance(tgt.pos)) * 0.01905) .. "m"
+        else
+            distance = math.ceil((ply:GetPos():Distance(tgt.pos)) * 0.0625) .. "ft"
+        end
+    else
+        if m then
+            distance = distance .. "m"
+        else
+            distance = distance .. "ft"
+        end
+    end
+    return "An enemy player is currently " .. distance .. " away!"
+end
+
+function VISION:Condition()
+    t, inno, i, m, j = player.AreTeamsLiving()
+    return t or i or m or j
+end
+
+Oracle:register(VISION)
