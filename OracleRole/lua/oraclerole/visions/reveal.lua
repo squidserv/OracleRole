b50df6000bed6c0e7d31285f10d7c44d6192f2ee
squidserv <mprovost1992@gmail.com>
2021-10-11 20:59:22 -0400

    Too many modes, catch outside values

--- OracleRole/lua/oraclerole/visions/reveal.lua
+++ OracleRole/lua/oraclerole/visions/reveal.lua
@@ -4,7 +4,7 @@ VISION.Name = "One of these players is bad."
 VISION.id = "reveal"
 
 CreateConVar("ttt_oracle_reveal_players", 4, FCVAR_NONE, "The max number of players selected that could be bad.")
-CreateConVar("ttt_oracle_reveal_mode", 3, FCVAR_NONE, "0 = One of, 1 = At least one of, 2 = Either")
+CreateConVar("ttt_oracle_reveal_mode", 2, FCVAR_NONE, "0 = One of, 1 = At least one of, 2 = Either")
 
 function VISION:GetVision()
 
@@ -15,7 +15,7 @@ function VISION:GetVision()
     local mode = GetConVar("ttt_oracle_reveal_mode"):GetInt()
     local oracle = player.GetLivingRole(ROLE_ORACLE)
 
-    if mode == 2 then
+    if mode > 1 or mode < 0 then
         mode = math.random(0,1)
     end
 
