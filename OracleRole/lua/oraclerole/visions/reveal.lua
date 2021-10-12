44c507e5c77e306327b2d8671f83ec81d8596cb0
squidserv <mprovost1992@gmail.com>
2021-10-11 21:24:36 -0400

    Table doesn't start at 0?

--- OracleRole/lua/oraclerole/visions/reveal.lua
+++ OracleRole/lua/oraclerole/visions/reveal.lua
@@ -4,7 +4,7 @@ VISION.Name = "One of these players is bad."
 VISION.id = "reveal"
 
 CreateConVar("ttt_oracle_reveal_players", 4, FCVAR_NONE, "The max number of players selected that could be bad.")
-CreateConVar("ttt_oracle_reveal_mode", 2, FCVAR_NONE, "0 = One of, 1 = At least one of, 2 = Either")
+CreateConVar("ttt_oracle_reveal_mode", 3, FCVAR_NONE, "1 = One of, 2 = At least one of, 3 = Either")
 
 function VISION:GetVision()
 
@@ -15,14 +15,14 @@ function VISION:GetVision()
     local mode = GetConVar("ttt_oracle_reveal_mode"):GetInt()
     local oracle = player.GetLivingRole(ROLE_ORACLE)
 
-    if mode > 1 or mode < 0 then
-        mode = math.random(0,1)
+    if mode > 2 or mode < 1 then
+        mode = math.random(1,2)
     end
 
     for _, ply in ipairs(player.GetAll()) do
         if i >= max and bad then break end
         if not oracle:IsSameTeam(ply) then
-            if not bad or mode == 1 then
+            if not bad or mode == 2 then
                 table.insert(plys, ply)
             end
             bad = true
@@ -36,7 +36,7 @@ function VISION:GetVision()
     local s = strings[mode]
 
     for _, p in ipairs(plys) do
-        s = s .. p:Nick() .. " "
+        s = s .. p:Nick() .. ", "
     end
 
     return s .. "is bad."
